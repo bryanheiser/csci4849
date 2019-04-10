7e13f92494e9e0d771721a10eb080f8b1e6ab964
Bryan Heiser <bryanheiser@Bryans-MacBook-Pro-10.local>
2019-04-10 13:30:06 -0600

    assignment9

---
+++ assignment9/assignment9.js
@@ -0,0 +1,39 @@
+function backInBoat() {
+  alert("Get Me Back In The Boat");
+}
+
+function imOK() {
+  alert("I'm OK");
+}
+
+function anotherLoop() {
+  alert("Make Another Loop");
+}
+
+function done() {
+  alert("I'm Done");
+}
+
+function left() {
+  alert("Go Left");
+}
+
+function right() {
+  alert("Go Right");
+}
+
+function faster() {
+  alert("Faster");
+}
+
+function slower() {
+  alert("Slower");
+}
+
+function justRight() {
+  alert("Just Right");
+}
+
+function injured() {
+  alert("Possible Injury");
+}
